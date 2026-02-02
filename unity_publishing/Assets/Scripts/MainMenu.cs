3182391eadeccc126c2c83172a37e338bba73a09
audreym101 <m.audrey1@alustudent.com>
2026-02-02 12:10:04 -0800

    Clean commit without large files

---
+++ unity_publishing/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+
+    public void PlayMaze()
+    {
+        // Load the Maze scene. Replace "MazeSceneName" with the actual name of your scene.
+        SceneManager.LoadScene("Maze");
+    }
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game"); // Log message for the editor
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Stop playing in the editor
+#else
+            Application.Quit(); // Quit the application
+#endif
+    }
+}
---
+++ unity_publishing/Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    void Update()
+    {
+        // Rotate the coin on the X axis over time
+        transform.Rotate(45f * Time.deltaTime, 0f, 0f);
+    }
+}
